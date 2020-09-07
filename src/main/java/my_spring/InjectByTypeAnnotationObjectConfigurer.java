d30d57e7395f1f04a52e5d06491a2869e906e6b3
kirkondrash <kirkondrash@yandex.ru>
2020-09-07 16:30:44 +0300

    my_spring hollywood

---
+++ src/main/java/my_spring/InjectByTypeAnnotationObjectConfigurer.java
@@ -0,0 +1,25 @@
+package my_spring;
+
+import heroes.RandomUtil;
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class InjectByTypeAnnotationObjectConfigurer implements ObjectConfigurer {
+    @Override
+    @SneakyThrows
+    public void configure(Object t) {
+        Class<?> implClass = t.getClass();
+        Field[] fields = implClass.getDeclaredFields();
+        for (Field field : fields) {
+            InjectByType annotation = field.getAnnotation(InjectByType.class);
+            if (annotation != null) {
+                field.setAccessible(true);
+                field.set(t, ObjectFactory.getInstance().createObject(field.getType()));
+            }
+        }
+    }
+}
